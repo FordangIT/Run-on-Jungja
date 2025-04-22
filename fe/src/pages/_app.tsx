1df803c7dfe320b7d156d3e84e745266a4fb13d9
FordangIT <fordang0819@gmail.com>
2025-04-22 16:11:33 +0900

    Chore: 구글 애널리틱스

--- fe/src/pages/_app.tsx
+++ fe/src/pages/_app.tsx
@@ -1,6 +1,24 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import Script from "next/script";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      {/* Google Analytics */}
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-49J4Z79J4K"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-49J4Z79J4K');
+        `}
+      </Script>
+      <Component {...pageProps} />;
+    </>
+  );
 }
